061f432826c350b133b8d99533d4a8792ed98140
25LP3069 25LP3069 <25LP3069@admins-iMac.local>
2025-10-29 19:32:24 +0800

    commit message

---
+++ src/app/_components/AdminCard.js
@@ -0,0 +1,25 @@
+"use client";
+
+import { DownIcon } from "../icons/DownIcon";
+
+export const AdminCard = (props) => {
+  const { customer, food, date, total, delivery, state, number } = props;
+
+  return (
+    <div className="border border-gray-100 w-full h-[56px] flex items-center ">
+      <button className="w-[20px] h-[20px] border border-black rounded-[5px] ml-3 "></button>
+      <p className="w-[56px] ml-10">{number}</p>
+      <p className="text-gray-500 w-[56px] ml-3">{customer}</p>
+      <p className="text-gray-500  w-[213px] ml-60 ">{food}</p>
+      <div>
+        <DownIcon />
+      </div>
+      <p className="text-gray-500 w-[160px] ml-10">{date}</p>
+      <div></div>
+      <p className="text-gray-500 w-[160px] ml-10">{total}</p>
+      <p className="text-gray-500 w-[213px] ml-10 h-[56px]">{delivery}</p>
+      <p className="text-gray-500 w-[160px] ml-10"></p>
+      <div></div>
+    </div>
+  );
+};
---
+++ src/app/admin/page.js
@@ -0,0 +1,90 @@
+"use client";
+import { AdminCard } from "../_components/AdminCard";
+import { DateIcon } from "../icons/Date";
+import { DownIcon } from "../icons/DownIcon";
+import { MenuIcon } from "../icons/Menu";
+import { NomNom } from "../icons/NomNom";
+import { OrderIcon } from "../icons/Order";
+import { UpDown } from "../icons/UpDown";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-100 flex gap-[5%]">
+      <div className="w-[250px] h-screen bg-white">
+        <div className="flex items-center gap-3 pt-5 justify-center">
+          <NomNom />
+          <div>
+            <p className="font-bold text-2xl">NomNom</p>
+            <p className="text-[#71717A]">Swift delivery</p>
+          </div>
+        </div>
+        <div className="flex justify-center pt-10">
+          <button className="flex justify-center gap-3 cursor-pointer">
+            <MenuIcon />
+            <p>Food Menu</p>
+          </button>
+        </div>
+        <div className="flex justify-center pt-10">
+          <button
+            className={`w-[165px] h-[40px] rounded-full flex justify-center items-center gap-5 cursor-pointer ${
+              true ? "bg-black text-white" : "bg-white text-black"
+            }`}
+          >
+            <OrderIcon />
+            <p className="text-white">Orders</p>
+          </button>
+        </div>
+      </div>
+      <div className="bg-white w-[70%] h-250 mt-[3%] rounded-lg border border-gray-200 overflow-y-scroll">
+        <div className="flex justify-between">
+          <div className="pt-2.5 pl-4">
+            <p className="font-bold text-2xl">Order</p>
+            <p className="text-gray-500">32 items</p>
+          </div>
+          <div className="flex items-center gap-3.5 pr-3.5">
+            <div className="w-[300px] h-[36px] border border-gray-300 rounded-full flex justify-center gap-3 items-center">
+              <DateIcon />
+              <p>13 June 2023 - 14 July 2023</p>
+            </div>
+            <button className="bg-gray-300 w-[179px] h-[36px] rounded-full text-white">
+              Change delivery state
+            </button>
+          </div>
+        </div>
+        <div className="border border-gray-100 w-full h-[56px] flex items-center ">
+          <button className="w-[20px] h-[20px] border border-black rounded-[5px] ml-3 "></button>
+          <p className="w-[56px] ml-10">№</p>
+          <p className="text-gray-500 w-[56px] ml-3">Customer</p>
+          <p className="text-gray-500  w-[213px] ml-60 ">Food</p>
+          <div></div>
+          <p className="text-gray-500 w-[160px] ml-10">Date</p>
+          <div>
+            <UpDown />
+          </div>
+          <p className="text-gray-500 w-[160px] ml-10">Total</p>
+          <p className="text-gray-500 w-[213px] ml-10">Delivery Address</p>
+          <p className="text-gray-500 w-[160px] ml-10">Delivery State</p>
+          <div>
+            <UpDown />
+          </div>
+        </div>
+        <AdminCard
+          number={1}
+          customer={"Test@gamil.com"}
+          food={"2 foods"}
+          date={"2024/12/20"}
+          total={"$26.97"}
+          delivery={
+            "2024/12/СБД, 12-р хороо, СБД нэгдсэн эмнэлэг Sbd negdsen emneleg | 100 айлын гүүрэн гарцны хойд талд 4д ногоонСБД, 12-р хороо, СБД нэгдсэн эмнэлэг Sbd negdsen emneleg | 100 айлын гүүрэн гарцны хойд талд 4д ногоон20"
+          }
+        />
+        <AdminCard />
+        <AdminCard />
+        <AdminCard />
+        <AdminCard />
+        <AdminCard />
+        <AdminCard />
+      </div>
+    </div>
+  );
+}
